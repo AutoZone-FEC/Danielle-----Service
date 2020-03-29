c3ec32de4803744d6f61348cf9e350e2c0d9be0e
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-03-28 19:11:19 -0500

    added another local storage variable

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -69,6 +69,7 @@ class Navbar extends React.Component{
 
     itemSelect(item) {
         window.localStorage.setItem('productID',this.state.searchIDs[item])
+        window.localStorage.setItem('updated',false)
 
         this.setState({
             selectedItem: item,
